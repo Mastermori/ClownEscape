f16a6e57179aac9890ca907208892597f340ea97
Mastermori <mastermoriii@gmail.com>
2024-01-27 21:25:23 +0100

    addec warning to level transition missing next level

--- levels/components/level_transition.gd
+++ levels/components/level_transition.gd
@@ -3,6 +3,10 @@ extends Area3D
 @export var next_level: PackedScene
 
 
+func _ready():
+	if not next_level:
+		print("Warning: level transition has no next level")
+
 func _on_body_entered(body):
 	if not is_instance_valid(next_level):
 		return
