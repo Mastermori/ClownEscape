9a60aee003c6e333fa15fb08c7e077fb6c77722b
Mastermori <mastermoriii@gmail.com>
2024-01-27 18:51:40 +0100

    added bounce correction to jump pad

--- obstacles/jump_pad/jump_pad.gd
+++ obstacles/jump_pad/jump_pad.gd
@@ -2,9 +2,11 @@ extends StaticBody3D
 
 # The speed at which the platform will propell a player away
 @export var bounce_height: float = 15.0
+@export var bounce_direction_correction: Vector3 = Vector3.ZERO
 # The time in seconds before the platform will bounce a player again
 # Do NOT set to values close to or below 0 as multiple collisions may occur
 @export var cooldown: float = 1.0
+## Set this to limit with which collision normal the pad bounces
 @export var collision_normal: Vector3
 # The countdown till the platform can propell the player again
 var countdown = 0
@@ -23,7 +25,7 @@ func collide_with_player(player: Player, collision: KinematicCollision3D):
 	if countdown > 0 or (collision_normal and not collision_normal.is_equal_approx(collision.get_normal())):
 		return
 	var bounce_velocity = player.get_jump_velocity(bounce_height)
-	player.velocity += collision.get_normal() * bounce_velocity
+	player.velocity += (collision.get_normal() + bounce_direction_correction).normalized() * bounce_velocity
 	countdown = cooldown
 	$CJumpPad/AnimationPlayer.play("default")
 	Global.play_sound_at(preload("res://player/Boing.ogg"), position)
