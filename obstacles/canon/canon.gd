362b98de722803235d5ccb881a98d62182550f5f
Mastermori <mastermoriii@gmail.com>
2024-01-27 21:25:38 +0100

    fixed canon timing

--- obstacles/canon/canon.gd
+++ obstacles/canon/canon.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var projectile_scene: PackedScene
 @export var push_strength: float = 40
 @export var fire_interval: float = 2
+## Will use fire interval as initial delay if <= 0
+@export var initial_delay: float = 0
 @export var fire_strength: float = 20
 @export var direction: Vector3 = Vector3(0, .5, 1)
 
@@ -10,12 +12,13 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.wait_time = fire_interval
-	$Timer.start()
+	$Timer.start(fire_interval if initial_delay <= 0 else initial_delay)
 
 func shoot():
 	shoot_in(direction, fire_strength)
 	Global.play_sound_at(preload("res://obstacles/canon/Cannon.ogg"), position)
+	if fire_interval != -1:
+		$Timer.start(fire_interval)
 
 func shoot_in(direction: Vector3, force: float):
 	var projectile := projectile_scene.instantiate() as RigidBody3D
