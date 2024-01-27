bd328d5046ee6aad7594d3ed05c1926a32123d10
Lievven <opti8298@gmx.de>
2024-01-27 04:38:26 +0100

    Fixed multiple simultaneous bounces bug. Implemented Dash functionality.

--- obstacles/bounce_pad.gd
+++ obstacles/bounce_pad.gd
@@ -1,6 +1,12 @@
 extends StaticBody3D
 
-const BOUNCE_VELOCITY = 10.0
+# The speed at which the platform will propell a player away
+@export var bounce_velocity = 15.0
+# The time in seconds before the platform will bounce a player again
+# Do NOT set to values close to or below 0 as multiple collisions may occur
+@export var cooldown = 1.0
+# The countdown till the platform can propell the player again
+var countdown = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +15,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	countdown = move_toward(countdown, 0, delta)
 
-
+# Called by the player object when a collision with the platform has been detected.
 func collide_with_player(player, collision):
-	player.velocity += collision.get_normal() * BOUNCE_VELOCITY
+	if countdown > 0:
+		return
+	player.velocity += collision.get_normal() * bounce_velocity
+	countdown = cooldown
--- player/player.gd
+++ player/player.gd
@@ -2,14 +2,18 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 8
+const JUMP_VELOCITY = 8.0
 const MOUSE_SENSITIVITY = 0.005
 const ACCELERATION = 15.0
+const DASH_COOLDOWN_DURATION = 1.0
+const DASH_VELOCITY = 15.0
+
+var camera = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var camera = null
+var dash_cooldown = 0.0
 
 func _ready():
 	camera = $Camera3D
@@ -34,7 +38,22 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
 		velocity.z = move_toward(velocity.z, 0, ACCELERATION * delta)
-
+		
+	if dash_cooldown > 0:
+		dash_cooldown -= delta
+	elif Input.is_action_pressed("dash"):
+		dash_cooldown = DASH_COOLDOWN_DURATION
+		# We can use one of these two transforms. The camera transform would dash EXACTLY where we are looking.
+		# However it needs to be limited since it can otherwise be used to fly.
+		# The vanilla transform only accelerates horizontally but can thus be used much more freely.
+		velocity -= camera.global_transform.basis.z * DASH_VELOCITY
+		#velocity -= transform.basis.z * DASH_VELOCITY
+	
+	# Here we finally process the movement. We use move_and_slide since we want to slide along surfaces.
+	# Additionally it allows for easy detection of whether we are grounded.
+	# We must still loop over all the collisions since some objects inflict additional bounces upon us.
+	# Theoretically we might be afflicted by multiple bounces at once, making them cancel each other out.
+	# But that's something that'll probably never happen so we take the faster approach. No writing new code.
 	move_and_slide()
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
@@ -44,6 +63,10 @@ func _physics_process(delta):
 
 
 func _input(event):
+	# Processing mouse movement here.
+	# We only want to look around while the mouse is "captured" inside godot.
+	# We rotate the character around the Y axis only since we do not want to flip ourselves upside down.
+	# The camera is rotated between looking straight up or straight down.
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
