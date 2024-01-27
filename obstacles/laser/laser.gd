7c0c43beb62d9e941a015b1a3876d5113cab6251
Mastermori <mastermoriii@gmail.com>
2024-01-27 22:06:44 +0100

    added max length for lasers

--- obstacles/laser/laser.gd
+++ obstacles/laser/laser.gd
@@ -10,6 +10,13 @@ extends Node3D
 		beam_particles.visible = val
 		set_physics_process(val)
 
+@export var max_length = 50 :
+	set(val):
+		max_length = val
+		if not is_node_ready():
+			return
+		ray.target_position = ray.target_position.limit_length(val)
+
 
 @onready var beam_mesh = %BeamMesh
 @onready var hit_particles = %HitParticles
@@ -18,6 +25,7 @@ extends Node3D
 
 func _ready():
 	active = active
+	max_length = max_length
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -29,16 +37,18 @@ func _physics_process(delta):
 		if collider is Player:
 			Global.level.reset_player()
 		cast_point = ray.to_local(ray.get_collision_point())
-		
-		beam_mesh.mesh.height = cast_point.y
-		beam_mesh.position.y = cast_point.y/2
-		
-		hit_particles.position.y = cast_point.y
-		
-		beam_particles.position.y = cast_point.y/2
-		beam_particles.process_material.set_emission_box_extents(
-			Vector3(beam_mesh.mesh.top_radius, abs(cast_point.y)/2, beam_mesh.mesh.bottom_radius)
-		)
+	
+	if not cast_point:
+		cast_point = ray.target_position
+	beam_mesh.mesh.height = cast_point.y
+	beam_mesh.position.y = cast_point.y/2
+	
+	hit_particles.position.y = cast_point.y
+	
+	beam_particles.position.y = cast_point.y/2
+	beam_particles.process_material.set_emission_box_extents(
+		Vector3(beam_mesh.mesh.top_radius, abs(cast_point.y)/2, beam_mesh.mesh.bottom_radius)
+	)
 
 func toggle():
 	active = not active
