090c618e17828421a79eb569be2cbb37f894e111
Mastermori <mastermoriii@gmail.com>
2024-01-27 19:00:45 +0100

    made laser toggleable

--- obstacles/laser/laser.gd
+++ obstacles/laser/laser.gd
@@ -1,20 +1,34 @@
-extends RayCast3D
+extends Node3D
+
+@export var active: bool = true :
+	set(val):
+		active = val
+		if not is_node_ready():
+			return
+		beam_mesh.visible = val
+		hit_particles.visible = val
+		beam_particles.visible = val
+		set_physics_process(val)
 
 
-@onready var beam_mesh = $BeamMesh
-@onready var hit_particles = $HitParticles
-@onready var beam_particles = $BeamParticles
+@onready var beam_mesh = %BeamMesh
+@onready var hit_particles = %HitParticles
+@onready var beam_particles = %BeamParticles
+@onready var ray = $Ray
+
+func _ready():
+	active = active
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var cast_point
-	force_raycast_update()
+	ray.force_raycast_update()
 	
-	if is_colliding():
-		var collider = get_collider()
+	if ray.is_colliding():
+		var collider = ray.get_collider()
 		if collider is Player:
 			Global.level.reset_player()
-		cast_point = to_local(get_collision_point())
+		cast_point = ray.to_local(ray.get_collision_point())
 		
 		beam_mesh.mesh.height = cast_point.y
 		beam_mesh.position.y = cast_point.y/2
@@ -25,3 +39,6 @@ func _physics_process(delta):
 		beam_particles.process_material.set_emission_box_extents(
 			Vector3(beam_mesh.mesh.top_radius, abs(cast_point.y)/2, beam_mesh.mesh.bottom_radius)
 		)
+
+func toggle():
+	active = not active
