6d9ba92ce0c9c0a64bdae19c465a6c4934fb1948
Lievven <opti8298@gmx.de>
2024-01-28 10:53:07 +0100

    Run timer.

--- globals/global.gd
+++ globals/global.gd
@@ -10,6 +10,7 @@ var total_run_deaths: int = 0
 var break_times: Dictionary = {}
 var current_level_timer: float = 0.0
 var current_level_deaths: int = 0
+var paused: bool = false
 
 func player_died():
 	player.is_dead = true
@@ -24,6 +25,18 @@ func _ready():
 	add_child(ui_layer)
 	ui = ui_layer.get_child(0)
 
+
+func _process(delta):
+	if paused:
+		return
+	total_run_timer += delta
+	current_level_timer += delta
+	var millis = fmod(total_run_timer, 1) * 100
+	var seconds = fmod(total_run_timer, 60)
+	var minutes = total_run_timer / 60
+	var text = "%02d:%02d:%02d   ⏲️\n%d   🤡" % [minutes, seconds, millis, total_run_deaths]
+	ui.update_timer_display(text)
+	
 func play_sound_at(sound: AudioStream, position: Vector3, max_distance = 0.0, volume: float = 0.0, pitch: float = 1.0):
 	var audio_player: AudioStreamPlayer3D = AudioStreamPlayer3D.new()
 	audio_player.stream = sound
@@ -38,8 +51,9 @@ func play_sound_at(sound: AudioStream, position: Vector3, max_distance = 0.0, vo
 
 func change_level(new_level: PackedScene):
 	if not level.level_name == "":
-		break_times[level.level_name] = [current_level_timer, current_level_deaths]
-	
+		break_times[level.level_name] = [current_level_timer, total_run_timer, current_level_deaths, total_run_deaths]
+	current_level_timer = 0.0
+	current_level_deaths = 0
 	ui.play_fade_out()
 	await ui.anim_player.animation_finished
 	get_tree().change_scene_to_packed.call_deferred(new_level)
