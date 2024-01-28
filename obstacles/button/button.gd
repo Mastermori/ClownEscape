f13598af72dab824ed1330ea0025a2f567c4e7fd
Mastermori <mastermoriii@gmail.com>
2024-01-28 10:47:52 +0100

    fixed push button

--- obstacles/button/button.gd
+++ obstacles/button/button.gd
@@ -1,49 +1,39 @@
 extends Area3D
 
-#@export var cooldown: float = 10000000
-@export var is_toggled: bool = false
-@export var toggle_timer: float = 0
-#@export var remaining_cooldown: float = 0
+@export var toggleable: bool = false
+@export var initial_toggle_state: bool = false
+@export var toggle_cooldown: float = 0
 
 signal switch_toggled
 signal switch_off
 signal switch_on
 
+var is_toggled: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	#remaining_cooldown -= delta
-
-
-func reset():
-	is_toggled = false
+	if toggleable and initial_toggle_state:
+		toggle_button()
 
 func _on_body_entered(body):
-	#if remaining_cooldown > 0:
-	#	return
-	
-	if is_toggled:
+	if not toggleable and is_toggled:
+		return
+	if not $Timer.is_stopped():
 		return
 	
-	is_toggled = false
 	Global.play_sound_at(preload("res://obstacles/button/Click.ogg"), position, 0.0, 20.0)
-	$GameReadyPressurePlate/AnimationPlayer.play("default")
 	
-	if toggle_timer > 0:	
-		$Timer.wait_time = toggle_timer
-		$Timer.start()
-	else:
-		toggle_button()
-		
+	if toggle_cooldown > 0:
+		$Timer.start(toggle_cooldown)
+	toggle_button()
+
 func toggle_button():
 	#remaining_cooldown = cooldown
-	emit_signal("switch_toggled")
+	switch_toggled.emit()
+	is_toggled = not is_toggled
 	if is_toggled:
-		emit_signal("switch_on")
+		$GameReadyPressurePlate/AnimationPlayer.play("Springplate_004Action")
+		switch_on.emit()
 	else:
-		emit_signal("switch_off")
+		$GameReadyPressurePlate/AnimationPlayer.play_backwards("Springplate_004Action")
+		switch_off.emit()
